0807e14213ac07e12be92261d46d29530ca969b2
Doolly <treeland88@hotmail.com>
2018-05-20 15:20:48 +0900

    1차 기본틀
    될까?

--- EBS_robot_challenge.ino
+++ EBS_robot_challenge.ino
@@ -1,44 +1,25 @@
-#include <Wire.h>
-#include <Adafruit_MotorShield.h>
-#include "utility/Adafruit_MS_PWMServoDriver.h"
-#include <Servo.h>
-
 #define PWM_READ_PIN 7
-Adafruit_MotorShield AFMS = Adafruit_MotorShield();
-Adafruit_StepperMotor *myStepper = AFMS.getStepper(513, 2);  // to motor port #2 (M3 and M4)
-Servo myservo;
+#define PUMP_PIN 9
 
 
 unsigned long duration;
 
-int degree;
-int usec;
 void setup() {
+  Serial.begin(9600);
+  pinMode(PUMP_PIN, OUTPUT);
   pinMode(PWM_READ_PIN, INPUT);
-  Serial.begin(115200);
-  AFMS.begin();  // create with the default frequency 1.6KHz
-
-  myservo.attach(10);
-  myStepper->setSpeed(20);  // 20 rpm
 }
 
 
 void loop() {
-
   duration = pulseIn(PWM_READ_PIN, HIGH);
+  Serial.print("duration = ");
+  Serial.println(duration);
 
-  //  Serial.println(duration);
-  //  delay(50);
-
-
-  if (duration > 1500) {
-    myservo.writeMicroseconds(980); // 985~2004 is 0~90 degree
-    delay(200);
-    myservo.writeMicroseconds(1550);
-    delay(1000);
-    myStepper->step(341, FORWARD, DOUBLE); //2052 step is 360 degree
-    myStepper->release();
+  if (duration > 1700) {
+    digitalWrite(PUMP_PIN, HIGH);
   }
-
-
+  else {
+    digitalWrite(PUMP_PIN, LOW);
+  }
 }
